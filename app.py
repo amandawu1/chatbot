3d4439888ef2970b273107b0cddd3eb5e8ca4bbb
amandawu1 <amandawu@Amandas-MacBook-Air.local>
2025-02-20 16:25:15 -0500

    changed query

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ app = Flask(__name__)
 def hello_world():
    return jsonify({"text":'Hello from Koyeb - you reached the main page!'})
 
-# def google_search(query):
-#     url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={CX}"
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         results = response.json().get("items", [])
-#         summaries = [item["snippet"] for item in results[:5]]
-#         return " ".join(summaries)
-#     return "No relevant results found."
+def google_search(query):
+    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={GOOGLE_API_KEY}&cx={CX}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        results = response.json().get("items", [])
+        summaries = [item["snippet"] for item in results[:5]]
+        return " ".join(summaries)
+    return "No relevant results found."
 
 @app.route('/query', methods=['POST'])
 def main():
@@ -36,15 +36,15 @@ def main():
 
     print(f"Message from {user} : {message}")
     
-    # search_summary = google_search(message)
+    search_summary = google_search(message)
 
     # Generate a response using LLMProxy
     response = generate(
         model='4o-mini',
-        system='answer my question and add keywords',
-        # system='Summarize the following information and answer the query:',
-        query= message,
-        # query=search_summary,
+        # system='answer my question and add keywords',
+        system='Summarize the following information and answer the query highlighting keywords:',
+        # query= message,
+        query=search_summary,
         temperature=0.0,
         lastk=0,
         session_id='GenericSession' #take userID into account to create separate sessionIDs for multiple simultaneous users
